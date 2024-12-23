30b5918aa82b4c7ea22b0c345f79d24ce22165c7
hikaryd <86579751+hikaryd@users.noreply.github.com>
2024-12-23 04:11:49 +0300

    fixes

--- migrate-to-flakes.sh
+++ migrate-to-flakes.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Enable flakes
 mkdir -p ~/.config/nix
-echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
+echo "experimental-features = nix-command flakes" >~/.config/nix/nix.conf
 
-# Remove old channels
 nix-channel --remove home-manager
 nix-channel --remove catppuccin
 
-# Build and activate new configuration
-cd ~/dotfiles
 nix build .#homeConfigurations.hikary.activationPackage
 ./result/activate
