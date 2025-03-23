f387f78448e4bd59211968c4bfa83ac93b1963bd
Hikary <hikary@proton.me>
2025-03-23 17:14:46 +0300

    Удалить скрипт run-vpn.sh и заменить на hiddify
    chore(hyprland): Заменил вызов run-vpn.sh на sudo hiddify
    chore(hyprland): Упростил правило для special:vpn в Hyprland

--- scripts/run-vpn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CURRENT_ENV=$(env | grep -E "DISPLAY|WAYLAND|XDG|DBUS|SESSION|GTK|QT|DESKTOP|HOME|USER|SHELL|PATH|TERM|COLOR")
-
-sudo -E dbus-launch --sh-syntax bash -c "$CURRENT_ENV hiddify"
