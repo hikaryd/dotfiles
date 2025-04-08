82b52eeababd3bb91a1d487a016c2744ee8a39ee
hikaryd <86579751+hikaryd@users.noreply.github.com>
2025-04-08 18:55:39 +0300

    Feat: Обновление Wayland конфигурации и стилей
    style(anyrun): Обновлены стили для темной темы anyrun
    feat(hyprpaper): Добавлен модуль hyprpaper для обоев
    feat(niri): Добавлены настройки для niri, анимации и стартовые команды
    chore(theme): Удален hyprpaper из списка пакетов для темы

--- home-manager/modules/wayland/niri.nix
+++ home-manager/modules/wayland/niri.nix
@@ -17,13 +17,12 @@
 
       inactiveBorderColor = "rgb(80 80 80)";
       focusRingColor = "rgb(66 153 225)";
-      inactiveFocusRingColor = inactiveBorderColor;
-      shadowColor = "#0000004C";
 
-      inactiveOpacity = 0.1;
+      inactiveOpacity = 0.7;
 
       monitorInternal = "eDP-1";
       monitorExternal = "HDMI-A-1";
+      makeCommand = command: { command = [ command ]; };
 
     in {
       outputs = {
@@ -55,36 +54,89 @@
         };
       };
 
-      animations = { enable = true; };
+      animations = {
+        enable = true;
+
+        shaders.window-resize = ''
+          vec4 resize_color(vec3 coords_curr_geo, vec3 size_curr_geo) {
+            vec3 coords_next_geo = niri_curr_geo_to_next_geo * coords_curr_geo;
+
+            vec3 coords_stretch = niri_geo_to_tex_next * coords_curr_geo;
+            vec3 coords_crop = niri_geo_to_tex_next * coords_next_geo;
+
+            // We can crop if the current window size is smaller than the next window
+            // size. One way to tell is by comparing to 1.0 the X and Y scaling
+            // coefficients in the current-to-next transformation matrix.
+            bool can_crop_by_x = niri_curr_geo_to_next_geo[0][0] <= 1.0;
+            bool can_crop_by_y = niri_curr_geo_to_next_geo[1][1] <= 1.0;
+
+            vec3 coords = coords_stretch;
+            if (can_crop_by_x)
+                coords.x = coords_crop.x;
+            if (can_crop_by_y)
+                coords.y = coords_crop.y;
+
+            vec4 color = texture2D(niri_tex_next, coords.st);
+
+            // However, when we crop, we also want to crop out anything outside the
+            // current geometry. This is because the area of the shader is unspecified
+            // and usually bigger than the current geometry, so if we don't fill pixels
+            // outside with transparency, the texture will leak out.
+            //
+            // When stretching, this is not an issue because the area outside will
+            // correspond to client-side decoration shadows, which are already supposed
+            // to be outside.
+            if (can_crop_by_x && (coords_curr_geo.x < 0.0 || 1.0 < coords_curr_geo.x))
+                color = vec4(0.0);
+            if (can_crop_by_y && (coords_curr_geo.y < 0.0 || 1.0 < coords_curr_geo.y))
+                color = vec4(0.0);
+
+            return color;
+          }
+        '';
+        prefer-no-csd = true;
+        hotkey-overlay.skip-at-startup = true;
+      };
 
       layout = {
-        gaps = 5;
-        tab-indicator = {
+        focus-ring.enable = false;
+        border = {
           enable = true;
           width = 1;
           active.color = focusRingColor;
           inactive.color = inactiveBorderColor;
         };
-        focus-ring = {
-          enable = false;
-          width = 2;
-          active.color = focusRingColor;
-          inactive.color = inactiveFocusRingColor;
+        shadow = { enable = true; };
+        preset-column-widths = [
+          { proportion = 0.25; }
+          { proportion = 0.5; }
+          { proportion = 0.75; }
+          { proportion = 1.0; }
+        ];
+        default-column-width = { proportion = 0.5; };
+        gaps = 6;
+        struts = {
+          left = 0;
+          right = 0;
+          top = 0;
+          bottom = 0;
         };
-        shadow = {
-          enable = true;
-          softness = 10;
-          spread = 5;
-          offset = {
-            x = 0;
-            y = 5;
-          };
-          color = shadowColor;
+        tab-indicator = {
+          hide-when-single-tab = true;
+          place-within-column = true;
+          position = "left";
+          corner-radius = 20.0;
+          gap = -12.0;
+          gaps-between-tabs = 10.0;
+          width = 4.0;
+          length.total-proportion = 0.1;
         };
       };
 
       input = {
-        focus-follows-mouse = { enable = true; };
+        focus-follows-mouse.enable = true;
+        warp-mouse-to-focus = true;
+        workspace-auto-back-and-forth = true;
         keyboard = {
           repeat-delay = 260;
           repeat-rate = 60;
@@ -95,9 +147,15 @@
         };
         mouse = { natural-scroll = false; };
         touchpad = {
-          natural-scroll = false;
+          click-method = "button-areas";
           dwt = true;
+          dwtp = true;
+          natural-scroll = false;
+          scroll-method = "two-finger";
           tap = true;
+          tap-button-map = "left-right-middle";
+          middle-emulation = true;
+          accel-profile = "adaptive";
         };
       };
 
@@ -121,28 +179,32 @@
         "other" = { open-on-output = "HDMI-A-1"; };
       };
       environment = {
-        DISPLAY = ":0";
-
-        QT_QPA_PLATFORM = "wayland;xcb";
-        QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
         QT_AUTO_SCREEN_SCALE_FACTOR = "1";
         QT_QPA_PLATFORMTHEME = "qt5ct";
         QT_SCALE_FACTOR_ROUNDING_POLICY = "RoundPreferFloor";
         QT_WAYLAND_DISABLED_INTERFACES = "wp_fractional_scale_manager_v1";
+        CLUTTER_BACKEND = "wayland";
+        DISPLAY = null;
+        GDK_BACKEND = "wayland,x11";
+        MOZ_ENABLE_WAYLAND = "1";
+        NIXOS_OZONE_WL = "1";
+        QT_QPA_PLATFORM = "wayland;xcb";
+        QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+        SDL_VIDEODRIVER = "wayland";
       };
 
       spawn-at-startup = [
-        { command = [ "/usr/libexec/polkit-gnome-authentication-agent-1" ]; }
-        { command = [ "${pkgs.waybar}/bin/waybar" ]; }
-        { command = [ "${pkgs.hyprpaper}/bin/hyprpaper" ]; }
-        { command = [ "${pkgs.kanata}/bin/kanata" ]; }
-        { command = [ "${pkgs.slack}/bin/slack" ]; }
-        { command = [ "dunst" ]; }
-        { command = [ "${pkgs.tidal-hifi}/bin/tidal-hifi" ]; }
-        { command = [ "sleep" "3" "&&" "notify-send" "123" ]; }
-        { command = [ "google-chrome-stable" ]; }
-        { command = [ "ayugram-desktop" ]; }
-        { command = [ "easyeffects --gapplication-service" ]; }
+        (makeCommand "/usr/libexec/polkit-gnome-authentication-agent-1")
+        (makeCommand "${pkgs.waybar}/bin/waybar")
+        (makeCommand "${pkgs.kanata}/bin/kanata")
+        (makeCommand "slack")
+        (makeCommand "dunst")
+        (makeCommand "${pkgs.tidal-hifi}/bin/tidal-hifi")
+        (makeCommand "google-chrome-stable")
+        (makeCommand "ayugram-desktop")
+        (makeCommand "easyeffects --gapplication-service")
+        (makeCommand "wl-paste --type image --watch cliphist store")
+        (makeCommand "wl-paste --type text --watch cliphist store")
         {
           command = [
             "${pkgs.dbus}/bin/dbus-update-activation-environment"
