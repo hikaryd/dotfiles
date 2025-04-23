f86c641290103174dd27c638ec31dfeca05c2217
hikaryd <86579751+hikaryd@users.noreply.github.com>
2025-04-23 15:57:20 +0300

    feat(waybar): Добавить Waybar
    Waybar интегрирован в окружение Wayland через модуль Home Manager.
    Добавлены пользовательские файлы конфигурации и стилей.
    Waybar заменяет hydepanel в автозапуске композитора Niri.
    Отключена старая настройка waybar.enable в модуле темы.

--- home-manager/modules/wayland/default.nix
+++ home-manager/modules/wayland/default.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }: {
-  imports = [ ./hyprland ./anyrun ./dunst.nix ./niri.nix ];
+  imports = [ ./hyprland ./anyrun ./dunst.nix ./niri.nix ./waybar.nix ];
 
   home.packages = with pkgs; [ glib wlroots xwayland libinput ];
 }
