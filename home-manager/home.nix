a1aa430d0f80394eac83a3e223e98d9ef4ebe28c
hikaryd <86579751+hikaryd@users.noreply.github.com>
2025-04-29 23:33:58 +0300

    chore: Исключить тему на macOS
    Настроить импорт модулей Home Manager в зависимости от операционной системы.
    Модуль темы специфичен для окружения Linux и может вызывать конфликты или быть неактуальным на macOS.
    Условный импорт обеспечивает переносимость конфигурации между системами.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,14 +1,19 @@
 { pkgs, inputs, system, lib, ... }: {
   home = {
     username = if system == "x86_64-linux" then "hikary" else "user";
-    homeDirectory = 
-      if system == "x86_64-linux" 
-      then "/home/hikary" 
-      else "/Users/user";
+    homeDirectory =
+      if system == "x86_64-linux" then "/home/hikary" else "/Users/user";
     stateVersion = "24.11";
   };
 
-  imports = [ ./modules ./theme.nix ./packages.nix ];
+  imports = if system == "x86_64-linux" then [
+    ./modules
+    ./packages.nix
+    ./theme.nix
+  ] else [
+    ./modules
+    ./packages.nix
+  ];
 
   programs.home-manager.enable = true;
 
