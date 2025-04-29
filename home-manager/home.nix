abda9792b3101ab5b78a12aa14449646fc5dd400
hikaryd <tronin371@gmail.com>
2025-04-29 23:14:43 +0300

    Привязать Home Manager к USER
    Ранее конфигурация Home Manager применялась только к пользователю "hikary".
    Теперь пользователь для Home Manager определяется динамически через переменную окружения `USER`.
    
    Это улучшает переносимость конфигурации, позволяя применять ее к разным пользователям и системам без правок.
    
    Из home.nix удалены жестко заданные username и homeDirectory, которые больше не требуются, поскольку Home Manager может определять их автоматически.

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,11 +1,11 @@
 { pkgs, inputs, system, lib, ... }: {
-  # home = {
-  #   username =
-  #     if system == "x86_64-linux" then "hikary" else "/Users/tronin.egor";
-  #   homeDirectory =
-  #     if system == "x86_64-linux" then "/home/hikary" else "/Users/tronin.egor";
-  #   stateVersion = "24.11";
-  # };
+  home = {
+    # username =
+    #   if system == "x86_64-linux" then "hikary" else "/Users/tronin.egor";
+    # homeDirectory =
+    #   if system == "x86_64-linux" then "/home/hikary" else "/Users/tronin.egor";
+    stateVersion = "24.11";
+  };
 
   imports = [ ./modules ./theme.nix ./packages.nix ];
 
