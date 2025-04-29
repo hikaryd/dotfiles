e3f02aebe322514f532bd2abdf7c6154d44aa6fa
hikaryd <tronin371@gmail.com>
2025-04-30 00:09:06 +0300

    refactor(ghostty): Перевести на xdg config
    Изменена настройка Ghostty с опции programs.ghostty.settings на прямое управление файлами через xdg.configFile.
    Это обеспечивает большую гибкость в настройке, включая добавление пользовательских тем.
    Добавлен файл темы stylix.

--- home-manager/modules/ghostty.nix
+++ home-manager/modules/ghostty.nix
@@ -1,32 +1,59 @@
-{ lib, ... }: {
-  programs.ghostty = {
-    enable = false;
+{ pkgs, system, ... }: {
+  home.packages = with pkgs;
+    if system == "x86_64-linux" then [ kanata ] else [ ];
 
-    settings = {
-      background-opacity = lib.mkForce 0.8;
-      minimum-contrast = 1.1;
-      font-size = 10;
-      # font = "Fira Code";
-      term = "xterm-256color";
-      bold-is-bright = true;
+  xdg.configFile = {
+    "ghostty/config" = {
+      text = ''
+        background-opacity = 0.8
+        minimum-contrast = 1.1
+        font-size = 10
+        term = xterm-256color
+        bold-is-bright = true
 
-      command = "${../../scripts/tmux-session.sh}";
-      linux-cgroup = "single-instance";
+        command = ${../../scripts/tmux-session.sh}
+        linux-cgroup = single-instance
 
-      theme = "stylix";
-      background-blur-radius = 20;
+        theme = stylix
+        background-blur-radius = 20
 
-      confirm-close-surface = false;
-      mouse-hide-while-typing = true;
+        confirm-close-surface = false
+        mouse-hide-while-typing = true
 
-      window-padding-x = 10;
-      window-padding-y = 10;
-      window-padding-balance = true;
-      window-inherit-working-directory = true;
-      window-inherit-font-size = true;
+        window-padding-x = 10
+        window-padding-y = 10
+        window-padding-balance = true
+        window-inherit-working-directory = true
+        window-inherit-font-size = true
 
-      scrollback-limit = 1000000;
-      custom-shader-animation = true;
+        scrollback-limit = 1000000
+        custom-shader-animation = true
+      '';
+    };
+
+    "ghostty/themes/stylix" = {
+      text = ''
+        background = 11121d
+        cursor-color = a0a8cd
+        foreground = a0a8cd
+        palette = 0=#11121d
+        palette = 1=#ee6d85
+        palette = 2=#95c561
+        palette = 3=#d7a65f
+        palette = 4=#7199ee
+        palette = 5=#a485dd
+        palette = 6=#9fbbf3
+        palette = 7=#a0a8cd
+        palette = 8=#353945
+        palette = 9=#ee6d85
+        palette = 10=#95c561
+        palette = 11=#d7a65f
+        palette = 12=#7199ee
+        palette = 13=#a485dd
+        palette = 14=#9fbbf3
+        palette = 15=#bcc2dc
+        selection-background = 212234
+      '';
     };
   };
 }
